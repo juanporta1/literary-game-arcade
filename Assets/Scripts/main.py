c4a5809b2a54d8159369f25a613e4e37f6045697
JuanPorta14 <juanporta.c@gmail.com>
2024-09-16 20:54:30 -0300

    commit de seguridad

--- Assets/Scripts/main.py
+++ Assets/Scripts/main.py
@@ -6,15 +6,15 @@ import globalVars
 
 class Window(arcade.Window):
     global globalVars
-    def __init__(self,width,height,title):
-        super().__init__(width,height,title)
+    def __init__(self,width,height,title,fullscreen=False):
+        super().__init__(width,height,title,fullscreen=fullscreen)
         globalVars.ACTUAL_WIDTH = self.width
         globalVars.ACTUAL_HEIGHT = self.height
 
 
 if __name__ == '__main__':
     
-    window = Window(1280,720,"Habia una vez...")
+    window = Window(1280,720,"Habia una vez...",True)
     menu = MenuView(window)
     
     window.show_view(menu)
